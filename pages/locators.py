6d418c96c791eccacb08cd1b4565aa7d8a442857
zografap <zograf.anton@yandex.ru>
2021-04-13 11:19:54 +0700

    добавлен класс LoginPageLocators и др.

--- pages/locators.py
+++ pages/locators.py
@@ -5,4 +5,10 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASS = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PROVE_PASS = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BTN = (By.NAME, "registration_submit")
     
--- test_main_page.py
+++ test_main_page.py
@@ -6,8 +6,5 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+def should_be_login_link(self):
+    assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
